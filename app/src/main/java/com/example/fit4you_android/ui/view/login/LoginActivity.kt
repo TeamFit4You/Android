5f3316a9407f3d995c15b810e20dee67feeaa357
wbham <wbham97@naver.com>
2023-03-31 03:08:42 +0900

    [Refact] #9 - 패키지 구조 변경
    - Fit4You
      ├── ui
      │   ├── base
      │   ├── view
      │   │   ├── basicstatuscheck
      │   │   │     └── questions
      │   │   ├── login
      │   │   └── signup
      │   └── viewmodel
    - 위와 같이 패키지 구조 변경

--- app/src/main/java/com/example/fit4you_android/ui/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/fit4you_android/ui/view/login/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fit4you_android
+package com.example.fit4you_android.ui.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fit4you_android.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
