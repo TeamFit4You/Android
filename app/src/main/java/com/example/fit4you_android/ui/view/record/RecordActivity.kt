02b26317ffe7d858cf1be456ce0b706e3ba82112
wbham <wbham97@naver.com>
2023-04-05 22:58:01 +0900

    [Add] 추천운동, 오늘의 운동, 운동 기록 activity 추가

---
+++ app/src/main/java/com/example/fit4you_android/ui/view/record/RecordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fit4you_android.ui.view.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fit4you_android.R
+
+class RecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+}
---
+++ app/src/main/java/com/example/fit4you_android/ui/view/today/TodayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fit4you_android.ui.view.today
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fit4you_android.R
+
+class TodayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_today)
+    }
+}
