911a6344c1f5c78f69e7c5b06120d8bd570bd376
wbham <wbham97@naver.com>
2023-04-06 11:44:38 +0900

    [Add] Main
    - base Activity & Fragment & ViewModel 추가

---
+++ app/src/main/java/com/example/fit4you_android/ui/base/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.fit4you_android.ui.base
+
+import androidx.lifecycle.ViewModel
+//import org.cazait.cazait_android.usecase.errors.ErrorManager
+//import javax.inject.Inject
+
+open class BaseViewModel : ViewModel() {
+//    @Inject
+//    lateinit var errorManager: ErrorManager
+}
