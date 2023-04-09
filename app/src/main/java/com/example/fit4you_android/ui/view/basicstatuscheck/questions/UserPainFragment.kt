6d78b5c29157f9f66f424c606e62d5d33cfe7a7f
wbham <wbham97@naver.com>
2023-04-09 19:19:36 +0900

    tmp save

---
+++ app/src/main/java/com/example/fit4you_android/ui/view/basicstatuscheck/questions/UserPainFragment.kt
@@ -0,0 +1,29 @@
+package com.example.fit4you_android.ui.view.basicstatuscheck.questions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.fit4you_android.R
+import com.example.fit4you_android.databinding.FragmentUserPainBinding
+import com.example.fit4you_android.ui.base.BaseFragment
+
+class UserPainFragment : BaseFragment<FragmentUserPainBinding, UserPainViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.fragment_user_pain
+    override val viewModel: UserPainViewModel by viewModels()
+
+    override fun initBeforeBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
