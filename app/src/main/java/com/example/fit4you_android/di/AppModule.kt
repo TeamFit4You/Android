5bc0bc52849c0124b0a82bcccb6b16c83c1e9a8a
wbham <wbham97@naver.com>
2023-05-05 19:53:37 +0900

    [Feat] #29 - DI 작업
    - 의존성 주입

---
+++ app/src/main/java/com/example/fit4you_android/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.example.fit4you_android.di
+
+import android.content.Context
+import com.example.fit4you_android.network.Network
+import com.example.fit4you_android.network.NetworkConnectivity
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCoroutineContext(): CoroutineContext {
+        return Dispatchers.IO
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkConnectivity(@ApplicationContext context: Context): NetworkConnectivity {
+        return Network(context)
+    }
+}
