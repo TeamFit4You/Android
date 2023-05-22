89c2f2738c492f001256849c850ed0e623eba8d7
wbham <wbham97@naver.com>
2023-05-22 20:00:28 +0900

    [Feat] ##35 - 오늘의 운동 전문가 영상 layout 구성
    - 버튼의 크기 줄이고 videoview, seekbar btn_play,pause 추가
    - 운동시 주의사항은 recyclerview로 구현

--- app/src/main/java/com/example/fit4you_android/ui/view/today/start/ExampleActivity.kt
+++ app/src/main/java/com/example/fit4you_android/ui/view/today/start/ExampleActivity.kt
@@ -1,12 +1,24 @@
 package com.example.fit4you_android.ui.view.today.start
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.fit4you_android.R
+import com.example.fit4you_android.databinding.ActivityExampleBinding
+import com.example.fit4you_android.ui.base.BaseActivity
 
-class ExampleActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_example)
+class ExampleActivity : BaseActivity<ActivityExampleBinding, ExampleViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_example
+    override val viewModel: ExampleViewModel by viewModels()
+
+    override fun initBeforeBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+
+    override fun initView() {
+
     }
 }
